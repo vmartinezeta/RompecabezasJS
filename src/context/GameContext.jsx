f277e6e5a497b4fdd89630ee4da0c984756de8da
Víctor Martínez <vito.marodriguez@gmail.com>
2025-07-01 12:00:42 -0600

    Juego de Rompecabezas

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useRef, useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 import useSound from 'use-sound'
 import urlSound from "../audio/musica-fondo.mp3"
 import PropTypes from "prop-types"
@@ -13,28 +13,11 @@ export const useGame = () => {
     return context
 }
 
-let letrasGlobal = []
 
 export function GameProvider({ children }) {
     const [play, { stop }] = useSound(urlSound,{loop:true})
     const [toggleMusica, setToggleMusica] = useState(false)
-    const [letras, setLetras] = useState([])
-    const [index, setIndex] = useState(0)
-    const [letra, setLetra] = useState(null)
-    const tiempo = useRef(1000)
 
-
-    const ultima = (l) => {
-        setLetra(l)
-    }
-
-    const next = () => {
-        setIndex(index + 1)
-    }
-
-    const resetIndex = () => {
-        setIndex(0)
-    }
 
     const onToggleMusica = () => {
         const toggle = !toggleMusica
@@ -47,27 +30,9 @@ export function GameProvider({ children }) {
         setToggleMusica(toggle)
     }
 
-    const addLetra = (letra) => {
-        letrasGlobal = [...letrasGlobal, letra]
-        setLetras(letrasGlobal)
-    }
-
-    const reset = () => {
-        letrasGlobal = []
-        setLetras(letrasGlobal)
-    }
 
     return <GameContext.Provider value={{
-        letra,
-        letras,
-        index,
-        tiempo,
         onToggleMusica,
-        addLetra,
-        reset, 
-        next,
-        ultima,
-        resetIndex
     }}>
         {children}
     </GameContext.Provider>
